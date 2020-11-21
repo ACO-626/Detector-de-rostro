add81e380eb234cf918c53dc2b4894135b78169c
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-11-21 10:05:42 -0600

    Agregar archivos de proyecto.

---
+++ FormPrincipal.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Detector_de_rostro
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(355, 539);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(118, 56);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "Detectar";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(355, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(280, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb1.Location = new System.Drawing.Point(240, 45);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(102, 22);
+            this.lb1.TabIndex = 3;
+            this.lb1.Text = "Dispositivo:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(715, 408);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(771, 607);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Detector de rostro";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormPrincipal_FormClosing);
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FormPrincipal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace Detector_de_rostro
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+                comboBox1.Items.Add(device.Name);
+            comboBox1.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+                            
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[comboBox1.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        private void Device_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            foreach(Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics=Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Green, 10))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;
+         
+
+        }
+
+        private void FormPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(device.IsRunning)
+            {
+                device.Stop();
+            }
+        }
+    }
+}
